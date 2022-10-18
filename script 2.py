8cc7b74771ca9a35ceb4f89367fa5216ee31ee46
danyala1 <d.mayasov@mail.ru>
2022-10-18 22:30:52 +0400

    Добавлен класс итератор, и ф-и для скриптов

--- script 2.py
+++ script 2.py
@@ -19,3 +19,32 @@ def second_script(path: str) -> str:
             with open(os.path.join("second_dataset", element[2] + "_" + namefile[2]), "wb") as f:
                 f.write(text)
         content = True
+
+def iterator2(name: str) -> Optional[str]:
+    '''create a csv'''
+    names = os.listdir(os.path.join("dataset", name))
+    for i in range(len(names)):
+        yield (names[i]) # делаем итератор
+    return None
+
+
+class Iterator2_txt:
+    def __init__(self, name: str):
+        self.names = os.listdir(os.path.join("dataset", name))
+        self.limit = len(self.names)
+        self.counter = 0
+
+    def __next__(self):
+        if self.counter < self.limit:
+            self.counter += 1
+            return self.names[self.counter - 1]
+        else:
+            raise StopIteration
+
+
+def script_2(name: str) ->None:
+    '''create a csv'''
+    second_script(name)
+
+if __name__=="__main__":
+    script_2("rt")
