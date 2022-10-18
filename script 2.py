4fc021c755b6b9f089d67953d3e5bc34c281a181
danyala1 <d.mayasov@mail.ru>
2022-10-18 22:21:21 +0400

    Ф-я которая читает файл из csv и создает для него новый файл

---
+++ script 2.py
@@ -0,0 +1,21 @@
+import os
+from typing import Optional
+
+import csv
+
+
+def second_script(path: str) -> str:
+    #read csv and copy dataset .../1/0001.txt =.../1_0001.txt
+    with open("classmates.csv", "r") as fh:
+        reader = csv.reader(fh)  # (!) обратите внимание, что reader возвращает итератор
+        spisok = list(reader)  # поэтому мы делаем приведение к типу list
+    if os.path.isdir("second_dataset") == False: os.makedirs("second_dataset")
+    content = False
+    for element in spisok:
+        if content:
+            with open(element[0], "rb") as f:
+                text = f.read()
+                namefile = element[1].split("/")
+            with open(os.path.join("second_dataset", element[2] + "_" + namefile[2]), "wb") as f:
+                f.write(text)
+        content = True
