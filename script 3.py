712fc368f74f2f1f097590b9d6e4b992df48972b
danyala1 <d.mayasov@mail.ru>
2022-10-18 22:56:13 +0400

    добавление итератора

--- script 3.py
+++ script 3.py
@@ -20,3 +20,32 @@ def third_script(path:str) ->str:
                 f.write(text)
             directory = os.path.join(out_directory, "third_dataset", str(name)+".txt")
             file_writer.writerow([directory, os.path.join("second_dataset", element), element[0]])
+
+def iterator3(name: str) -> str:
+    '''create a csv'''
+    names = os.listdir(os.path.join("dataset", name))
+    for i in range(len(names)):
+        yield (names[i]) # делаем итератор
+    return None
+
+
+class Iterator3_txt:
+    def __init__(self, name: str):
+        self.names = os.listdir(os.path.join("dataset", name))
+        self.limit = len(self.names)
+        self.counter = 0
+
+    def __next__(self):
+        if self.counter < self.limit:
+            self.counter += 1
+            return self.names[self.counter - 1]
+        else:
+            raise StopIteration
+
+
+def script_3(name: str) -> None:
+    '''create a csv'''
+    third_script(name)
+
+if __name__=="__main__":
+    script_3("rt")
