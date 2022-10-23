2b66f16957b6ec1f906b687c0581e7caaa691b3d
danyala1 <d.mayasov@mail.ru>
2022-10-23 20:05:39 +0400

    Убрал ф-ю для итераторов из 3-х скриптов

--- script 1.py
+++ script 1.py
@@ -17,26 +17,6 @@ def first_script(path_dir: str) -> None:
                 for element in files:
                     file_writer.writerow([str(dirs) + "/" + element, "dataset"+'/' + str(star) + "/" + element, star])
 
-def iterator1(name: str) -> Optional[str]:
-    """create a csv"""
-    names = os.listdir(os.path.join("dataset", name))
-    for i in range(len(names)):
-        yield (names[i])  # делаем итератор
-    return None
-
-class Iterator1_txt:
-    def __init__(self, name: str):
-        self.names = os.listdir(os.path.join("dataset", name))
-        self.limit = len(self.names)
-        self.counter = 0
-
-    def __next__(self):
-        if self.counter < self.limit:
-            self.counter += 1
-            return self.names[self.counter - 1]
-        else:
-            raise StopIteration
-
 def script_1(name: str) -> None:
     """create a csv"""
     first_script(name)
--- script 2.py
+++ script 2.py
@@ -14,33 +14,11 @@ def second_script(path: str) -> str:
     for element in spisok:
         if content:
             with open(element[0], "rb") as f:
-                text = f.read()
+                text = f.read()                  #в кажжом элементе из списка он копирует его с именем звезда, и именем 2
                 namefile = element[1].split("/")
             with open(os.path.join("second_dataset", element[2] + "_" + namefile[2]), "wb") as f:
                 f.write(text)
         content = True
-
-def iterator2(name: str) -> Optional[str]:
-    '''create a csv'''
-    names = os.listdir(os.path.join("dataset", name))
-    for i in range(len(names)):
-        yield (names[i]) # делаем итератор
-    return None
-
-
-class Iterator2_txt:
-    def __init__(self, name: str):
-        self.names = os.listdir(os.path.join("dataset", name))
-        self.limit = len(self.names)
-        self.counter = 0
-
-    def __next__(self):
-        if self.counter < self.limit:
-            self.counter += 1
-            return self.names[self.counter - 1]
-        else:
-            raise StopIteration
-
 
 def script_2(name: str) ->None:
     '''create a csv'''
